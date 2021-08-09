1f37dab5ec47d6c09ce60513c28c2eec7d187cbf
manzim <manzim.ruet@gmail.com>
2021-08-09 16:48:52 +0600

    modified CRUD todoList

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import 'tachyons/css/tachyons.min.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    
+      <App />
+    
+  </React.StrictMode>,
+  document.getElementById('root')
+);
